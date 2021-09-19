bf8f50a731f90663fc724e3e4e486b5996f16b9c
yuban10703 <yuban10703@foxmail.com>
2021-09-20 04:38:26 +0800

    fix 无数据时返回

--- bot_word_cloud/database/get_words.py
+++ bot_word_cloud/database/get_words.py
@@ -6,4 +6,4 @@ from ._shared import tmpDB
 def get_words(groupid) -> list:
     if data_tmp := tmpDB.table("wordcloud").get(where("group") == groupid):  # 如果有数据
         return data_tmp["words"]
-    return ['无']
+    return ['无数据']
