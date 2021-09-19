d98c5af82dfbc7ef425777544de3b9ebd7dfb97c
yuban10703 <yuban10703@foxmail.com>
2021-09-20 02:22:00 +0800

    添加stopwords 增加:每天21点开始向所有在bot运行期间说过话的群发送当日词云

--- bot_word_cloud/__init__.py
+++ bot_word_cloud/__init__.py
@@ -8,11 +8,19 @@ from botoy.collection import MsgTypes
 from botoy.schedule import scheduler
 
 from .database import log_words, reset_database
-from .word_cloud import build_word_cloud_pic
+from .word_cloud import build_word_cloud_pic, send_to_all_group
 
 __doc__ = "词云"
 
 jieba.initialize()
+
+scheduler.add_job(
+    send_to_all_group,
+    'cron',
+    hour=21,
+    minute=00,
+    misfire_grace_time=30,
+)  # 每天21点把词云发送到所有群
 
 scheduler.add_job(
     reset_database,
@@ -20,7 +28,7 @@ scheduler.add_job(
     hour=00,
     minute=00,
     misfire_grace_time=30,
-)
+)  # 每天凌晨重置数据库
 
 
 @deco.ignore_botself
@@ -34,5 +42,8 @@ async def receive_group_msg(ctx: GroupMsg):
         logger.success('词云')
     else:
         words = jieba.lcut(re.sub(r'\[表情\d+]', '', ctx.Content))  # 过滤表情,并分词
-        logger.success(f"[{ctx.FromGroupName}:{ctx.FromGroupId}] ; [{ctx.FromNickName}:{ctx.FromUserId}] 分词-->{words}")
+        # words_finish = del_stopwords(words)  # 去除stopwords
+        logger.success(
+            f"[{ctx.FromGroupName}:{ctx.FromGroupId}] ; [{ctx.FromNickName}:{ctx.FromUserId}] 分词-->{words}"
+        )
         log_words(ctx.FromGroupId, words)
--- bot_word_cloud/word_cloud.py
+++ bot_word_cloud/word_cloud.py
@@ -1,16 +1,17 @@
 import base64
-
+import time
 from io import BytesIO
 from pathlib import Path
-import imageio
 
+from botoy import Action, jconfig, logger
 from wordcloud import WordCloud
 
-from .database import get_words
+from .database import get_words, get_all_groups
 
 curFileDir = Path(__file__).absolute().parent
 
 # mk = imageio.imread(curFileDir / "1.png")
+stopwords = [line.strip() for line in open(curFileDir / "stopwords.txt", encoding='UTF-8').readlines()]
 
 
 def build_word_cloud_pic(groupid):
@@ -19,15 +20,25 @@ def build_word_cloud_pic(groupid):
         height=550,
         scale=2,
         background_color='white',
-        stopwords={'的', '我'},
+        stopwords=set(stopwords),
         # mask=mk,
         # font_path=str(curFileDir / '方正黑体简体.ttf')
         # max_font_size=100,  # 设置字体最大值
         # random_state=50,  # 设置随机生成状态，即多少种配色方案
-        font_path=str(curFileDir / 'msyh.ttc')
+        font_path="C:\Windows\Fonts\msyh.ttc"
     )
     word_cloud.generate(' '.join(get_words(groupid)))
     img = word_cloud.to_image()
     with BytesIO() as bf:
         img.save(bf, format="PNG")
         return base64.b64encode(bf.getvalue()).decode()
+
+
+def send_to_all_group():
+    logger.warning('开始向有数据的群发送今日词云')
+    action = Action(qq=jconfig.bot)
+    groups = get_all_groups()
+    for group in groups:
+        logger.warning(f'开始向群{group}发送今日词云')
+        action.sendGroupPic(group, picBase64Buf=build_word_cloud_pic(group))
+        time.sleep(3)
