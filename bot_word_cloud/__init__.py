842f389c87239a59e714dab03977e0556f4efd7f
yuban10703 <yuban10703@foxmail.com>
2021-09-20 19:35:29 +0800

    优化代码

--- bot_word_cloud/__init__.py
+++ bot_word_cloud/__init__.py
@@ -1,4 +1,3 @@
-import asyncio
 import re
 from pathlib import Path
 
@@ -6,6 +5,7 @@ import jieba
 from botoy import GroupMsg, logger, S
 from botoy import async_decorators as deco
 from botoy.collection import MsgTypes
+from botoy.contrib import async_run
 from botoy.schedule import scheduler
 
 from .database import log_words, reset_database
@@ -15,20 +15,23 @@ __doc__ = "词云"
 curFileDir = Path(__file__).absolute().parent
 
 # jieba.initialize()
-stopwords = [line.strip() for line in open(curFileDir / "stopwords.txt", encoding='UTF-8').readlines()]
+stopwords = [
+    line.strip()
+    for line in open(curFileDir / "stopwords.txt", encoding="UTF-8").readlines()
+]
 jieba.load_userdict(str(curFileDir / "dict.txt"))
 
 scheduler.add_job(
     send_to_all_group,
-    'cron',
-    hour=21,
-    minute=00,
+    "cron",
+    hour=19,
+    minute=34,
     misfire_grace_time=30,
 )  # 每天21点把词云发送到所有群
 
 scheduler.add_job(
     reset_database,
-    'cron',
+    "cron",
     hour=00,
     minute=00,
     misfire_grace_time=30,
@@ -36,18 +39,26 @@ scheduler.add_job(
 
 
 @deco.ignore_botself
-@deco.these_msgtypes(MsgTypes.TextMsg, MsgTypes.AtMsg, MsgTypes.PicMsg, MsgTypes.ReplyMsgA, MsgTypes.ReplyMsg)
+@deco.these_msgtypes(
+    MsgTypes.TextMsg,
+    MsgTypes.AtMsg,
+    MsgTypes.PicMsg,
+    MsgTypes.ReplyMsgA,
+    MsgTypes.ReplyMsg,
+)
 async def receive_group_msg(ctx: GroupMsg):
-    if ctx.Content == '词云':
-        # word_cloud_pic_b64 = build_word_cloud_pic(ctx.FromGroupId)
-        loop = asyncio.get_event_loop()
-        word_cloud_pic_b64 = await loop.run_in_executor(None, build_word_cloud_pic, ctx.FromGroupId)
+    if ctx.Content == "词云":
+        word_cloud_pic_b64 = await build_word_cloud_pic(ctx.FromGroupId)
         await S.aimage(word_cloud_pic_b64)
-        logger.success('词云')
+        logger.success("词云")
     else:
         if msg := parser_msg(ctx):
-            words = jieba.lcut(re.sub(r'\[表情\d+]', '', msg))  # 过滤表情,并分词
-            words_finish = [word for word in words if word not in stopwords]  # 去除stopwords
+            words = await async_run(
+                jieba.lcut, re.sub(r"\[表情\d+]", "", msg)
+            )  # 过滤表情,并分词
+            words_finish = [
+                word for word in words if word not in stopwords
+            ]  # 去除stopwords
             logger.success(
                 f"[{ctx.FromGroupName}:{ctx.FromGroupId}] ; [{ctx.FromNickName}:{ctx.FromUserId}] 分词-->{words_finish}"
             )
--- bot_word_cloud/word_cloud.py
+++ bot_word_cloud/word_cloud.py
@@ -1,3 +1,4 @@
+import asyncio
 import base64
 import re
 import time
@@ -5,7 +6,9 @@ from io import BytesIO
 from pathlib import Path
 
 import ujson as json
-from botoy import Action, jconfig, logger, GroupMsg
+from botoy import Action, jconfig, logger
+from botoy import GroupMsg
+from botoy.contrib import to_async
 from botoy.parser import group as gp
 from wordcloud import WordCloud
 
@@ -16,37 +19,47 @@ curFileDir = Path(__file__).absolute().parent
 
 # mk = imageio.imread(curFileDir / "1.png")
 
+
 def parser_msg(ctx: GroupMsg):
     if msg_data := gp.reply(ctx) or gp.pic(ctx):
-        return re.sub(
-            "@.* ", "", msg_data.Content
-        )
+        return re.sub("@.* ", "", msg_data.Content)
     elif ctx.MsgType == "ReplayMsg":
         msg = json.loads(ctx.Content)
-        return re.sub(
-            f"@{msg['UserExt'][0]['QQNick']}\\s+", "", msg["Content"]
-        )
+        return re.sub(f"@{msg['UserExt'][0]['QQNick']}\\s+", "", msg["Content"])
     elif msg_data := gp.at(ctx) or ctx:
         return msg_data.Content
+    # if data := gp.pic(ctx):
+    #     print('pic')
+    # elif data := gp.at(ctx):
+    #     print('at')
+    #     msg = re.sub(data.UserExt["QQNick"], '', data.Content)
+    # elif data := gp.reply(ctx):
+    #     print('rep')
+    #     msg = re.sub(data.UserExt["QQNick"], '', data.Content)
+    # else:
+    #     print('?')
+    #     msg = ctx.Content
+    # print(msg)
+    # return msg
 
 
+@to_async
 def build_word_cloud_pic(groupid):
     word_cloud = WordCloud(
         width=500,
         height=500,
         scale=2,
-        max_words=800,
-        max_font_size=230,
-        prefer_horizontal=1.0,
-        background_color='white',
+        max_words=700,
+        max_font_size=270,
+        background_color="white",
         # stopwords=set(stopwords),
         # mask=mk,
         # font_path=str(curFileDir / '方正黑体简体.ttf')
         # max_font_size=100,  # 设置字体最大值
         # random_state=50,  # 设置随机生成状态，即多少种配色方案
-        font_path=str(curFileDir / 'LXGWWenKai-Regular.ttf')
+        font_path=str(curFileDir / "msyh.ttc"),
     )
-    word_cloud.generate(' '.join(get_words(groupid)))
+    word_cloud.generate(" ".join(get_words(groupid)))
     img = word_cloud.to_image()
     with BytesIO() as bf:
         img.save(bf, format="PNG")
@@ -54,10 +67,12 @@ def build_word_cloud_pic(groupid):
 
 
 def send_to_all_group():
-    logger.warning('开始向有数据的群发送今日词云')
+    logger.warning("开始向有数据的群发送今日词云")
     action = Action(qq=jconfig.bot)
     groups = get_all_groups()
     for group in groups:
-        logger.warning(f'开始向群{group}发送今日词云')
-        action.sendGroupPic(group, picBase64Buf=build_word_cloud_pic(group))
+        logger.warning(f"开始向群{group}发送今日词云")
+        action.sendGroupPic(
+            group, picBase64Buf=asyncio.run(build_word_cloud_pic(group))
+        )
         time.sleep(3)
