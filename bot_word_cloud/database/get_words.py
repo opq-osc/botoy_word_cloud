2093d6a7c89477528850efe6bd1b61aaaf61b0e6
yuban10703 <2846021566@qq.com>
2021-09-21 16:47:17 +0800

    优化参数

--- bot_word_cloud/database/get_words.py
+++ bot_word_cloud/database/get_words.py
@@ -12,5 +12,5 @@ def get_words(groupid) -> dict:
     if data_tmp:
         for k in set(data_tmp["words"]):
             freq_dict[k] = data_tmp["words"].count(k)
-        return dict(sorted(freq_dict.items(), key=lambda d: d[1], reverse=True)[:200])  # 按照value从大到小排序
+        return dict(sorted(freq_dict.items(), key=lambda d: d[1], reverse=True)[:100])  # 按照value从大到小排序
     return {'无': 1}
